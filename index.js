782e4af5b3f32f008d92609fad35c633a593852d
ChidiogoVictor <chidiogovictor10@gmail.com>
2024-05-01 00:53:03 +0100

    added second random number

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-// random number between 1 and 6
+// random number1 between 1 and 6
 var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1 * 7;
-randomNumber1 = Math.floor(randomNumber1);
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
 
 // random dice image
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 var randomImageSource = "images/" + randomDiceImage;
 
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource);
+var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
-var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
+// random number2 between 1 and 6
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
