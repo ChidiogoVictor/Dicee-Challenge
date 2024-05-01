516fc164d60b1d4b72cb21de952242f230d7fdd8
ChidiogoVictor <chidiogovictor10@gmail.com>
2024-05-01 12:24:59 +0100

    added conditional statement

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ randomNumber1 = Math.floor(randomNumber1) + 1;
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 var randomImageSource = "images/" + randomDiceImage;
 
-var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 // random number2 between 1 and 6
 var randomNumber2 = Math.random();
@@ -18,3 +18,14 @@ var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 var randomImageSource2 = "images/" + randomDiceImage2;
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// changes the text in h1 with respect to player that got a higher dice number.
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!🚩";
+}
+else {
+    document.querySelector("h1").innerHTML = "🚩Its a Draw!🚩";
+}
