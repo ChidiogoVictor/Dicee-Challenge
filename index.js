5f553220923426e5cb463ed69b80fa02ff7dc5f9
ChidiogoVictor <chidiogovictor10@gmail.com>
2024-05-02 14:36:36 +0100

    refactored my js code

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 // random number1 between 1 and 6
-var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1 * 6;
-randomNumber1 = Math.floor(randomNumber1) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 // random dice image
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = "images/" + "dice" + randomNumber1 + ".png";
 
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 // random number2 between 1 and 6
-var randomNumber2 = Math.random();
-randomNumber2 = randomNumber2 * 6;
-randomNumber2 = Math.floor(randomNumber2) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-var randomImageSource2 = "images/" + randomDiceImage2;
+var randomImageSource2 = "images/" + "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
